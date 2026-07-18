3d569f65beab9371c4bdaae37a9b805f285719d4
Aadarsh Aggarwal <aadi314159@gmail.com>
2026-07-18 10:56:51 -0700

    Added MCreator support. old java folder is useless

---
+++ java-mcreator/src/main/java/com/nobigcorps/strawberrymc/init/StrawberrymcModDispenseBehaviors.java
@@ -0,0 +1,17 @@
+/*
+ *	MCreator note: This file will be REGENERATED on each build.
+ */
+package com.nobigcorps.strawberrymc.init;
+
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+
+@EventBusSubscriber
+public class StrawberrymcModDispenseBehaviors {
+	@SubscribeEvent
+	public static void init(FMLCommonSetupEvent event) {
+		event.enqueueWork(() -> {
+		});
+	}
+}
