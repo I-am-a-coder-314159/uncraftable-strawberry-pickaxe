7d7cf919698413fe8e5268d9e00700b196a505cf
Aadarsh Aggarwal <aadi314159@gmail.com>
2026-08-30 10:05:48 -0700

    commits to move to next repository

--- java-mcreator/src/main/java/com/nobigcorps/strawberrymc/procedures/CalculateRadiationDamageProcedureProcedure.java
+++ java-mcreator/src/main/java/com/nobigcorps/strawberrymc/procedures/CalculateRadiationDamageProcedureProcedure.java
@@ -13,10 +13,21 @@ public class CalculateRadiationDamageProcedureProcedure {
 		double dirY = 0;
 		double dirZ = 0;
 		double currentLength = 0;
+		double thrY = 0;
+		double thrX = 0;
+		double thrZ = 0;
+		double distance = 0;
 		String blockInterceptList = "";
 		{
 			final Vec3 _center = new Vec3(x, y, z);
 			for (Entity entityiterator : world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(10 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList()) {
+				dirX = x - entityiterator.getX();
+				dirY = y - entityiterator.getY();
+				dirZ = z - entityiterator.getZ();
+				currentLength = dirX * dirX + dirY * dirY + dirZ * dirZ;
+				thrX = dirX / (currentLength * 20);
+				thrY = dirY / (currentLength * 20);
+				thrZ = dirZ / (currentLength * 20);
 			}
 		}
 	}
